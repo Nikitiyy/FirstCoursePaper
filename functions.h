56f407d8d91d3b0dce7ec68a18ea2deb5b39c5ba
Nikitiyy <nikita184615@gmail.com>
2025-06-03 12:39:33 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,24 +4,21 @@
 #include <iostream>
 #include <Windows.h>
 #include <string>
-#include <cstring>
 #include <conio.h>
-#include <iomanip>
 
 
 using namespace std;
 
-void SetConsoleSize(int width, int height);
-void showArea(char area[][100], int gameCounter, int, string);
+void showArea(char area[][100], int gameCounter, int, int, string a = " ");
 void car(char area[][100], int lane);
 void delCar(char area[][100], int lane);
 void obstacle(char area[][100]);
 void heal(char area[][100]);
-void Lose(char area[][100]);
 bool pause();
 void title();
-int menu();
-void printElement(string[], int size, short x, short y, int color);
+void printLose();
+bool menu();
+void printElement(string[], int size, short x, short y, int color,int sleep = 0);
 void printRules();
 void pressKey(WORD keyCode);
 
